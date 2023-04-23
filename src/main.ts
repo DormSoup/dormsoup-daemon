3196f23e0f9e307f63df2f1fa7d5c921c4dca1b4
Chengyuan Ma <chengyuanma@protonmail.com>
2023-04-23 11:28:02 -0400

    feat: make OAuth2 work

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,84 @@
-import { ImapFlow } from "imapflow";
+import {
+    AuthorizationCodeRequest,
+    AuthorizationUrlRequest,
+    CryptoProvider,
+    PublicClientApplication
+} from "@azure/msal-node";
 import assert from "assert";
-import dotenv from "dotenv";
+import express from "express";
+import asyncHandler from "express-async-handler";
+import fs from "fs";
+import HttpStatus from "http-status-codes";
+import https from "https";
+import { ImapFlow } from "imapflow";
 
-dotenv.config();
+type AuthResult = { user: string; accessToken: string };
 
-const client = new ImapFlow({
-    host: "outlook.office365.com",
-    port: 993,
-    secure: true,
-    auth: {
-        user: process.env.MAIL_USER!!,
-        pass: process.env.MAIL_PASS
-    }
-});
+async function authenticate(): Promise<AuthResult> {
+    const clientConfig = {
+        auth: {
+            // Shamelessly pillaged from Thunderbird.
+            // See https://hg.mozilla.org/releases/comm-esr102/file/tip/mailnews/base/src/OAuth2Providers.jsm
+            clientId: "9e5f94bc-e8a4-4e73-b8be-63364c29d753",
+            authority: "https://login.microsoftonline.com/common"
+        }
+    };
+    const pca = new PublicClientApplication(clientConfig);
+    const scopes = ["https://outlook.office.com/IMAP.AccessAsUser.All"];
+    const redirectUri = `https://localhost`;
+    const cryptoProvider = new CryptoProvider();
+    const { verifier, challenge } = await cryptoProvider.generatePkceCodes();
 
-console.log(process.env.MAIL_USER!!);
+    const authCodeRequest: AuthorizationUrlRequest = {
+        scopes,
+        redirectUri,
+        codeChallenge: challenge,
+        codeChallengeMethod: "S256"
+    };
+    pca.getAuthCodeUrl(authCodeRequest)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => console.log(JSON.stringify(error)));
+    const app = express();
+    let resolve: (value: AuthResult | Promise<AuthResult>) => void;
+    const promise = new Promise<AuthResult>((res) => (resolve = res));
+    app.use(express.urlencoded({ extended: false }));
+    app.get(
+        "/",
+        asyncHandler(async (req, res) => {
+            const tokenRequest: AuthorizationCodeRequest = {
+                code: req.query.code as string,
+                scopes,
+                redirectUri,
+                codeVerifier: verifier,
+                clientInfo: req.query.client_info as string
+            };
+            const response = await pca.acquireTokenByCode(tokenRequest);
+            res.status(HttpStatus.OK).send(response.accessToken);
+            console.log(response.accessToken);
+            server.close(() => {
+                resolve({ user: response.account!.username, accessToken: response.accessToken });
+            });
+        })
+    );
+    const [privateKey, publicKey] = await Promise.all([
+        fs.promises.readFile("selfsigned.key", "utf8"),
+        fs.promises.readFile("selfsigned.crt", "utf8")
+    ]);
+    const server = https.createServer({ key: privateKey, cert: publicKey }, app);
+    server.listen(443);
+    return promise;
+}
 
 export default async function main() {
+    const client = new ImapFlow({
+        host: "outlook.office365.com",
+        port: 993,
+        secure: true,
+        auth: await authenticate()
+    });
+
     // Wait until client connects and authorizes
     await client.connect();
 
