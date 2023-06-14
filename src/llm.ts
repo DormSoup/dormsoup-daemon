2500c744de5594373536a91319793d3b54423e77
Chengyuan Ma <macy404@mit.edu>
2023-06-14 01:25:03 +0000

    Add comments and fixed the broken pipeline

--- src/llm.ts
+++ src/llm.ts
@@ -5,44 +5,44 @@ import HttpStatus from "http-status-codes";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
-export const CURRENT_MODEL_NAME = "GPT-3.5-051523";
+export const CURRENT_MODEL_NAME = "GPT-3.5-061223.12";
 
 export class Event {
-    public event: boolean = false;
-    public title: string = "unknown";
-    public dateTime: Date = new Date();
-    public location: string = "unknown";
-    public organizer: string = "unknown";
+  public event: boolean = false;
+  public title: string = "unknown";
+  public dateTime: Date | Date[] = new Date();
+  public location: string = "unknown";
+  public organizer: string = "unknown";
 }
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 assert(OPENAI_API_KEY !== undefined, "OPENAI_API_KEY environment variable must be set");
 
 const openai = new OpenAIApi(
-    new Configuration({
-        apiKey: OPENAI_API_KEY
-    })
+  new Configuration({
+    apiKey: OPENAI_API_KEY
+  })
 );
 
 const PROMPT_INTRO = dedent`
-    Identify the following items from the email below:
-    - Whether the email is inviting you to an event (true or false boolean value. For example, shows and talks are events, senior sales and club position applications are not events)
-    - The title of the event (up to five words)
-    - The dateTime of the event (in yyyy-MM-ddTHH:mm:ss format that can be recognized by JavaScript's Date constructor, the date received might help with your inference when the exact date is absent)
-    - The location of the event
-    - The organization hosting the event
+  Identify the following items from the email below:
+  - Whether the email is inviting you to an event (true or false boolean value. For example, shows and talks are events, senior sales and club position applications are not events)
+  - The title of the event (up to five words)
+  - The dateTime of the event (in yyyy-MM-ddTHH:mm:ss format that can be recognized by JavaScript's Date constructor, the date received might help with your inference when the exact date is absent)
+  - The location of the event
+  - The organization hosting the event
 
-    The email is delimited with triple backticks.
-    Format your response as a JSON object with the following keys (just JSON, with no extra explanations):
-    - "event"
-    - "title"
-    - "dateTime"
-    - "location"
-    - "organizer"
+  The email is delimited with triple backticks.
+  Format your response as a JSON object with the following keys (just JSON, with no extra explanations):
+  - "event"
+  - "title"
+  - "dateTime" (array format, if only one available time use array with only one element with no trailing comma, otherwise an array of multiple datetimes)
+  - "location"
+  - "organizer"
 
-    If the information is not present in the email, leave the value as "unknown".
+  If the information is not present in the email, leave the value as "unknown".
 
-    Email text:
+  Email text:
 `;
 
 /**
@@ -54,85 +54,100 @@ const PROMPT_INTRO = dedent`
  * @returns an Event object. If the email does not contain the information or the LLM made mistakes, the value is "unknown".
  */
 export async function extractFromEmail(
-    subject: string,
-    body: string,
-    dateReceived: Date,
-    debugMode: boolean = false
+  subject: string,
+  body: string,
+  dateReceived: Date,
+  debugMode: boolean = false
 ): Promise<Event> {
-    let response;
-    let backOff = 1000;
-    const assembledPrompt = dedent`
-        \`\`\`
-        Subject: ${subject}
-        Date Received: ${dateReceived}
-        Body:
-        ${body}
-        \`\`\`                
-    `;
+  let response;
+  let backOff = 1000;
 
-    if (debugMode) console.log("Assembled prompt:", assembledPrompt);
+  // Get rid of shitty base64.
+  body = body.replaceAll(/[A-Za-z0-9+\/=]{64,}/g, "");
 
-    while (true) {
-        response = await openai.createChatCompletion(
-            {
-                model: "gpt-3.5-turbo",
-                messages: [
-                    { role: "system", content: PROMPT_INTRO },
-                    { role: "user", content: assembledPrompt }
-                ]
-            },
-            { validateStatus: () => true }
-        );
-        if (response.status === HttpStatus.OK) break;
-        if (response.status === HttpStatus.TOO_MANY_REQUESTS) {
-            if (debugMode) console.warn(`Rate limited. Retrying in ${backOff} ms...`);
-            await new Promise((resolve) => setTimeout(resolve, backOff));
-            backOff *= 1.5;
-        } else
-            throw new Error(`OpenAI API call failed with status ${response.status}: ${response}`);
+  const assembledPrompt = dedent`
+    \`\`\`
+    Subject: ${subject}
+    Date Received: ${dateReceived}
+    Body:
+    ${body}
+    \`\`\`                
+  `;
+
+  if (debugMode) console.log("Assembled prompt:", assembledPrompt);
+
+  while (true) {
+    response = await openai.createChatCompletion(
+      {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: PROMPT_INTRO },
+          { role: "user", content: assembledPrompt }
+        ]
+      },
+      { validateStatus: () => true }
+    );
+    if (response.status === HttpStatus.OK) break;
+    if (response.status === HttpStatus.TOO_MANY_REQUESTS) {
+      if (debugMode) console.warn(`Rate limited. Retrying in ${backOff} ms...`);
+      await new Promise((resolve) => setTimeout(resolve, backOff));
+      backOff *= 1.5;
+    } else {
+      throw new Error(`OpenAI API call failed with status ${response.status}: ${response}`);
     }
-    const completion = response.data.choices[0];
-    assert(completion.finish_reason === "stop", "OpenAI API call failed");
-    const completionText = completion.message?.content;
-    assert(completionText !== undefined);
+  }
+  const completion = response.data.choices[0];
+  assert(completion.finish_reason === "stop", "OpenAI API call failed");
+  let completionText = completion.message?.content.trim();
+  assert(completionText !== undefined);
 
-    if (debugMode) console.log("Completion text:", completionText);
+  if (debugMode) console.log("Completion text:", completionText);
 
-    try {
-        const event: Event = JSON.parse(completionText);
-        for (const properties of Object.keys(new Event())) {
-            assert(properties in event, `The key ${properties} is not present in the LLM response`);
-            if (properties === "dateTime") {
-                event.dateTime = new Date(event.dateTime as unknown as string);
-                void event.dateTime.toISOString();
-            }
-        }
-        return event;
-    } catch {
-        console.log("Cannot parse JSON:", completionText);
-        return new Event();
-    }
+  return tryParseEventJSON(completionText);
 }
 
-// const testBody = `
-// Hi everyone,
+/**
+ * Tries to parse the JSON response from the LLM.
+ * This function does not throw. If anything goes wrong, it returns an empty Event object.
+ *
+ * @param completionText The completion text from LLM.
+ * @returns An event object.
+ */
+function tryParseEventJSON(completionText: string): Event {
+  let isEvent = false;
+  try {
+    // Removes surrounding ```s
+    if (completionText.startsWith("```") && completionText.endsWith("```"))
+      completionText = completionText.substring(3, completionText.length - 3);
+    // Removes trailing comma (hacky)
+    completionText = completionText.replaceAll(/,\s*\]/g, "]");
 
-// Please join the MIT Ballroom Dance Team for some good company and fun dancing at our Spring Fling Social this Saturday, April 1st from 7-11 pm in La Sala de Puerto Rico at the Student Center (W20). There will be a beginner lesson from 7-8 pm followed by open dancing 8-11 pm. No prior dance experience or partner is necessary!
+    const event: Event = JSON.parse(completionText);
+    isEvent = event.event;
+    // While theoretically the completion text should follow the schema of the Event object, 
+    // the object JSON.parse() isn't necessarily an actual Event object. We have to check here.
+    for (const properties of Object.keys(new Event())) {
+      assert(properties in event, `The key ${properties} is not present in the LLM response`);
 
-// See flyer below for more details. Hope to see you there!
-
-// [cid:image003.png@01D96212.1757BE60]
-
-// ---
-
-// Courtney Lunger
-// Publicity Coordinator
-// MIT Ballroom Dance Team
-
-// Bcc'ed to dorms, pink flower for bc-talk
-// `;
-
-// const testSubject = `[Macgregor] Spring Fling Social this Saturday, April 1st!`;
-// const testDateReceived = new Date("2023-03-29T21:00:00.000Z");
-
-// console.log(await extractFromEmail(testSubject, testBody, testDateReceived));
+      if (properties === "dateTime") {
+        // Most notably for date time, since JSON does not have a date type, it always come in as 
+        // string(s). 
+        if (typeof event.dateTime === "string") {
+          event.dateTime = new Date(event.dateTime as unknown as string);
+          // Sanity check: try toISOString() to see if it's a valid date. Invalid dates will throw
+          // RangeError here.
+          void event.dateTime.toISOString();
+        } else {
+          event.dateTime = (event.dateTime as unknown as string[]).map((date) => new Date(date));
+          event.dateTime.forEach((dateTime) => dateTime.toISOString());
+        }
+      }
+    }
+    return event;
+  } catch (error) {
+    // We only care about the error if the LLM thinks it's an event.
+    // Non-events aren't added to the database for now anyways.
+    if (isEvent) console.log("Cannot parse JSON:", completionText, error);
+    return new Event();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,195 +1,234 @@
-import { DataSource, PrismaClient } from "@prisma/client";
+import { DataSource, Email, EmailSender, PrismaClient } from "@prisma/client";
 import assert, { AssertionError } from "assert";
 import { convert } from "html-to-text";
 import { ImapFlow } from "imapflow";
-import { ParsedMail, simpleParser } from "mailparser";
+import { AddressObject, ParsedMail, simpleParser } from "mailparser";
 
 import { authenticate } from "./auth.js";
 import { CURRENT_MODEL_NAME, extractFromEmail } from "./llm.js";
 
-const LOOKBACK_DAYS = 7;
+const LOOKBACK_DAYS = 60;
 
 export default async function main() {
-    const auth = await authenticate();
-    const client = new ImapFlow({
-        host: "outlook.office365.com",
-        port: 993,
-        secure: true,
-        auth,
-        logger: false
+  const auth = await authenticate();
+  const client = new ImapFlow({
+    host: "outlook.office365.com",
+    port: 993,
+    secure: true,
+    auth,
+    logger: false
+  });
+
+  const prisma = new PrismaClient();
+  await client.connect();
+
+  let lock = await client.getMailboxLock("INBOX");
+  try {
+    assert(typeof client.mailbox !== "boolean");
+    console.log(`Mailbox has ${client.mailbox.exists} messages`);
+    const since = new Date();
+    since.setDate(new Date().getDate() - LOOKBACK_DAYS);
+    const allUids = await client.search({ since: since }, { uid: true });
+    // minUid: what is the earliest email received after `since`?
+    const minUid = Math.min(...allUids);
+    const byUserAndRecent = {
+      scrapedBy: auth.user,
+      uid: { gte: minUid }
+    };
+    // ignoredUids: emails that cannot be dormspams because they don't contain the keywords.
+    const ignoredUids = await prisma.ignoredEmail.findMany({
+      select: { uid: true },
+      where: byUserAndRecent
+    });
+    // processedUids: emails that have been processed by the current model.
+    const processedUids = await prisma.email.findMany({
+      select: { uid: true },
+      where: { ...byUserAndRecent, modelName: { equals: CURRENT_MODEL_NAME } }
     });
 
-    const prisma = new PrismaClient();
-    await client.connect();
-
-    let lock = await client.getMailboxLock("INBOX");
-    try {
-        assert(typeof client.mailbox !== "boolean");
-        console.log(`Mailbox has ${client.mailbox.exists} messages`);
-        const since = new Date();
-        since.setDate(new Date().getDate() - LOOKBACK_DAYS);
-        const allUids = await client.search({ since: since }, { uid: true });
-        // minUid: what is the earliest email received after `since`?
-        const minUid = Math.min(...allUids);
-        // ignoredUids: emails that cannot be dormspams because they don't contain the keywords.
-        const ignoredUids = await prisma.ignoredEmail.findMany({
-            select: { uid: true },
-            where: {
-                scrapedBy: auth.user,
-                uid: { gte: minUid }
-            }
-        });
-        // processedUids: emails that have been processed by the current model.
-        const processedUids = await prisma.email.findMany({
-            select: { uid: true },
-            where: {
-                scrapedBy: auth.user,
-                uid: { gte: minUid },
-                modelName: { equals: CURRENT_MODEL_NAME }
-            }
-        });
-        // seenUids: no need to look at these emails again. saves bandwidth and tokens.
-        const seenUids = ignoredUids.concat(processedUids).map((email) => email.uid);
-        const uids = allUids.filter((uid) => !seenUids.includes(uid));
-        console.log(`Received unseen ${uids.length} mails in the past ${LOOKBACK_DAYS} days`);
-        if (uids.length === 0) {
-            console.log("Nothing to do...");
-            return;
-        }
-        for await (let message of client.fetch(
-            uids,
-            {
-                uid: true,
-                envelope: true,
-                source: true
-            },
-            { uid: true, changedSince: 0n }
-        )) {
-            const parsed = await simpleParser(message.source);
-            await processMail(prisma, auth.user, message.uid, parsed);
-        }
-    } finally {
-        lock.release();
-        await prisma.$disconnect();
-        await client.logout();
+    // seenUids: no need to look at these emails again. saves bandwidth and tokens.
+    const seenUids = ignoredUids.concat(processedUids).map((email) => email.uid);
+    const uids = allUids.filter((uid) => !seenUids.includes(uid));
+    console.log(`Received unseen ${uids.length} mails in the past ${LOOKBACK_DAYS} days`);
+    if (uids.length === 0) {
+      console.log("Nothing to do...");
+      return;
     }
+
+    // We need not fetch these processed emails to save bandwidth.
+    const fetchedEmails = await prisma.email.findMany({
+      where: { ...byUserAndRecent, modelName: { not: CURRENT_MODEL_NAME } },
+      include: { sender: true }
+    });
+    const fetchedUids = fetchedEmails.map((email) => email.uid);
+
+    const mailProcessors: Promise<void>[] = [];
+    for await (let message of client.fetch(
+      uids.filter((uid) => !fetchedUids.includes(uid)),
+      { uid: true, envelope: true, source: true },
+      { uid: true, changedSince: 0n }
+    )) {
+      mailProcessors.push(
+        simpleParser(message.source).then((parsed) =>
+          processMail(prisma, auth.user, message.uid, parsed)
+        )
+      );
+    }
+
+    mailProcessors.push(
+      ...fetchedEmails.map((email) =>
+        processMail(prisma, auth.user, email.uid, emailToRelaxedParsedMail(email))
+      )
+    );
+
+    await Promise.all(mailProcessors);
+  } finally {
+    lock.release();
+    await prisma.$disconnect();
+    await client.logout();
+  }
 }
 
-function isDormspam(parsed: ParsedMail): boolean {
-    // See https://how-to-dormspam.mit.edu/.
-    const dormspamKeywords = [
-        "bcc'd to all dorms",
-        "bcc's to all dorms",
-        "bcc'd to dorms",
-        "bcc'ed dorms",
-        "bcc'ed to dorms",
-        "bcc to dorms",
-        "bcc'd to everyone",
-        "bcc dormlists",
-        "bcc to dormlists",
-        "for bc-talk"
-    ];
-    return dormspamKeywords.some((keyword) => parsed.text?.includes(keyword));
+type RelaxedParsedMail = Omit<ParsedMail, "attachments" | "headers" | "headerLines" | "from"> & {
+  from?: Omit<AddressObject, "html" | "text"> | undefined;
+};
+
+function isDormspam(parsed: RelaxedParsedMail): boolean {
+  // See https://how-to-dormspam.mit.edu/.
+  const dormspamKeywords = [
+    "bcc'd to all dorms",
+    "bcc's to all dorms",
+    "bcc'd to dorms",
+    "bcc'ed dorms",
+    "bcc'ed to dorms",
+    "bcc to dorms",
+    "bcc'd to everyone",
+    "bcc dormlists",
+    "bcc to dormlists",
+    "for bc-talk"
+  ];
+  return dormspamKeywords.some((keyword) => parsed.text?.includes(keyword));
+}
+
+function emailToRelaxedParsedMail(email: Email & { sender: EmailSender }): RelaxedParsedMail {
+  return {
+    messageId: email.messageId,
+    from: {
+      value: [
+        {
+          address: email.sender.email,
+          name: email.sender.name ?? email.sender.email
+        }
+      ]
+    },
+    html: email.body,
+    subject: email.subject,
+    date: email.receivedAt,
+    inReplyTo: email.inReplyToId ?? undefined,
+    text: convert(email.body)
+  };
 }
 
 async function processMail(
-    prisma: PrismaClient,
-    scrapedBy: string,
-    uid: number,
-    parsed: ParsedMail
+  prisma: PrismaClient,
+  scrapedBy: string,
+  uid: number,
+  parsed: RelaxedParsedMail
 ): Promise<void> {
-    const receivedAt = parsed.date ?? new Date();
-    try {
-        assert(isDormspam(parsed));
-        const { messageId, from, html, subject } = parsed;
-        assert(messageId !== undefined && from !== undefined && html && subject !== undefined);
-        const sender = from.value[0];
-        assert(sender.address !== undefined);
-        const senderAddress = sender.address;
-        const senderName = sender.name ?? senderAddress;
+  const receivedAt = parsed.date ?? new Date();
+  let email: Email | undefined = undefined;
 
-        let inReplyTo = undefined;
-        if (parsed.inReplyTo !== undefined) {
-            const inReplyToEmail = await prisma.email.findUnique({
-                where: { messageId: parsed.inReplyTo }
-            });
-            assert(inReplyToEmail !== null);
-            inReplyTo = {
-                connect: { messageId: parsed.inReplyTo }
-            };
-        }
-        const email = await prisma.email.upsert({
-            where: { messageId },
-            create: {
-                messageId,
-                scrapedBy,
-                uid,
-                sender: {
-                    connectOrCreate: {
-                        where: { email: senderAddress },
-                        create: {
-                            email: senderAddress,
-                            name: senderName
-                        }
-                    }
-                },
-                subject,
-                body: html,
-                receivedAt,
-                modelName: CURRENT_MODEL_NAME,
-                inReplyTo
-            },
-            update: {
-                modelName: CURRENT_MODEL_NAME
-            }
-        });
+  try {
+    assert(isDormspam(parsed));
+    const { messageId, from, html, subject } = parsed;
+    assert(messageId !== undefined && from !== undefined && html && subject !== undefined);
+    const sender = from.value[0];
+    assert(sender.address !== undefined);
+    const senderAddress = sender.address;
+    const senderName = sender.name ?? senderAddress;
 
-        const text = parsed.text ?? convert(html);
-        const extractedEvent = await extractFromEmail(subject, text, receivedAt);
-        if (!extractedEvent.event) return;
-        // LLM may return malformed datetime.
-        void extractedEvent.dateTime.toISOString();
-
-        let root = email;
-        while (root.inReplyToId !== null) {
-            root =
-                (await prisma.email.findUnique({ where: { messageId: root.inReplyToId } })) ??
-                assert.fail("Thread root not in database");
-        }
-
-        const existing = await prisma.event.findFirst({ where: { fromEmailId: root.messageId } });
-        if (existing !== undefined) return;
-        prisma.event.create({
-            data: {
-                source: DataSource.DORMSPAM,
-                title: extractedEvent.title,
-                date: extractedEvent.dateTime,
-                location: extractedEvent.location,
-                organizer: extractedEvent.organizer,
-                fromEmail: {
-                    connect: {
-                        messageId: root.messageId
-                    }
-                }
-            }
-        });
-        console.log(`Registered email: ${parsed.subject}: `, extractedEvent);
-    } catch (error) {
-        if (error instanceof AssertionError || error instanceof RangeError) {
-            console.log(`Ignored email: ${parsed.subject} ${uid}`);
-            await prisma.ignoredEmail.upsert({
-                where: { scrapedBy_uid: { scrapedBy, uid } },
-                create: {
-                    scrapedBy,
-                    uid,
-                    receivedAt
-                },
-                update: {}
-            });
-        } else {
-            throw error;
-        }
+    let inReplyTo = undefined;
+    if (parsed.inReplyTo !== undefined) {
+      const inReplyToEmail = await prisma.email.findUnique({
+        where: { messageId: parsed.inReplyTo }
+      });
+      assert(inReplyToEmail !== null);
+      inReplyTo = {
+        connect: { messageId: parsed.inReplyTo }
+      };
     }
+    email = await prisma.email.upsert({
+      where: { messageId },
+      create: {
+        messageId,
+        scrapedBy,
+        uid,
+        sender: {
+          connectOrCreate: {
+            where: { email: senderAddress },
+            create: { email: senderAddress, name: senderName }
+          }
+        },
+        subject,
+        body: html,
+        receivedAt,
+        modelName: CURRENT_MODEL_NAME,
+        inReplyTo
+      },
+      update: { modelName: CURRENT_MODEL_NAME }
+    });
+
+    const text = parsed.text ?? convert(html);
+    const extractedEvent = await extractFromEmail(subject, text, receivedAt);
+    if (!extractedEvent.event) return;
+
+    let root = email;
+    while (root.inReplyToId !== null) {
+      root =
+        (await prisma.email.findUnique({ where: { messageId: root.inReplyToId } })) ??
+        assert.fail("Thread root not in database");
+    }
+
+    const existing = await prisma.event.findFirst({ where: { fromEmailId: root.messageId } });
+    if (existing !== null) return;
+
+    const dates =
+      extractedEvent.dateTime instanceof Date ? [extractedEvent.dateTime] : extractedEvent.dateTime;
+
+    await Promise.all(
+      dates.map((date) =>
+        prisma.event.create({
+          data: {
+            date,
+            source: DataSource.DORMSPAM,
+            title: extractedEvent.title,
+            location: extractedEvent.location,
+            organizer: extractedEvent.organizer,
+            fromEmail: { connect: { messageId: root.messageId } }
+          }
+        })
+      )
+    );
+
+    console.log(`Registered email: ${parsed.subject}: `, extractedEvent);
+  } catch (error) {
+    if (error instanceof AssertionError || error instanceof RangeError) {
+      console.log(`Ignored email: ${parsed.subject} ${uid}`);
+      await prisma.ignoredEmail.upsert({
+        where: { scrapedBy_uid: { scrapedBy, uid } },
+        create: { scrapedBy, uid, receivedAt },
+        update: {}
+      });
+    } else {
+      if (email !== undefined) {
+        await prisma.email.update({
+          where: { messageId: email.messageId },
+          data: { modelName: "" }
+        });
+      }
+      throw error;
+    }
+  }
 }
 
 await main();
