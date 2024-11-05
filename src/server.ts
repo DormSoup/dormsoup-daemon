d82b5873719af10114de49600ef7242fe49f1ee5
t11s <greenfeatherhelp@gmail.com>
2024-11-05 15:12:03 -0500

    nit: times

--- src/server.ts
+++ src/server.ts
@@ -22,10 +22,10 @@ const startServer = () => {
   setInterval(async () => {
     await pushToSubscribers();
     await flushEmbeddings();
-  }, 1000 * 60);
+  }, 1000 * 60); // Every minute.
   setInterval(async () => {
     await syncGCal();
-  }, 1000 * 60 * 30);
+  }, 1000 * 60 * 30); // Every 30 minutes.
 };
 
 startServer();
